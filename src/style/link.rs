ec29ff974dee151a61816940dbe7890f81497155
Wesley Parr <wesley@cryptoquick.com>
2020-01-29 22:43:48 -0700

    hover and selected styles

--- src/style/link.rs
+++ src/style/link.rs
@@ -10,28 +10,33 @@ impl button::StyleSheet for Link {
       Link::Nav { selected } => {
         if *selected {
           button::Style {
-            background: Some(Background::Color(Color::from_rgb(0.2, 0.2, 0.7))),
+            background: Some(Background::Color(Color::WHITE)), // 0, 0, 0, 0.8 is active black
             border_radius: 10,
-            text_color: Color::WHITE,
+            text_color: Color::from_rgb(0.10, 0.10, 0.10),
             ..button::Style::default()
           }
         } else {
-          button::Style::default()
+          //button::Style::default()
+          button::Style {
+            text_color: Color::from_rgb(0.84, 0.84, 0.84),
+            ..button::Style::default()
+          }
         }
       }
     }
   }
 
   fn hovered(&self) -> button::Style {
-    let active = self.active();
-
-    button::Style {
-      text_color: match self {
-        Link::Nav { selected } if !selected => Color::from_rgb(0.2, 0.2, 0.7),
-        _ => active.text_color,
-      },
-      shadow_offset: active.shadow_offset + Vector::new(0.0, 1.0),
-      ..active
+    //let active = self.active();
+    match self {
+      Link::Nav { selected: _ } => {
+        button::Style {
+          // background: Some(Background::Color(Color::WHITE)), // 0, 0, 0, 0.8 is active black
+          // border_radius: 10,
+          text_color: Color::from_rgb(0.10, 0.10, 0.10),
+          ..button::Style::default()
+        }
+      }
     }
   }
 }
