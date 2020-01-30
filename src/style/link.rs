388c3e9878722ed5666c3b51e150d0afd817e031
Hunter Trujillo <hunter@cryptoquick.com>
2020-01-30 00:57:17 -0700

    Linter fixes.

--- src/style/link.rs
+++ src/style/link.rs
@@ -1,43 +1,43 @@
 use iced::{button, Color};
 
 pub enum Link {
-  Nav { selected: bool },
+    Nav { selected: bool },
 }
 
 impl button::StyleSheet for Link {
-  fn active(&self) -> button::Style {
-    match self {
-      Link::Nav { selected } => {
-        if *selected {
-          button::Style {
-            text_color: Color::from_rgb(0.10, 0.10, 0.10),
-            ..button::Style::default()
-          }
-        } else {
-          button::Style {
-            text_color: Color::from_rgb(0.84, 0.84, 0.84),
-            ..button::Style::default()
-          }
+    fn active(&self) -> button::Style {
+        match self {
+            Link::Nav { selected } => {
+                if *selected {
+                    button::Style {
+                        text_color: Color::from_rgb(0.10, 0.10, 0.10),
+                        ..button::Style::default()
+                    }
+                } else {
+                    button::Style {
+                        text_color: Color::from_rgb(0.84, 0.84, 0.84),
+                        ..button::Style::default()
+                    }
+                }
+            }
         }
-      }
     }
-  }
 
-  fn hovered(&self) -> button::Style {
-    match self {
-      Link::Nav { selected } => {
-        if *selected {
-          button::Style {
-            text_color: Color::from_rgb(0.10, 0.10, 0.10),
-            ..button::Style::default()
-          }
-        } else {
-          button::Style {
-            text_color: Color::from_rgb(0.40, 0.40, 0.40),
-            ..button::Style::default()
-          }
+    fn hovered(&self) -> button::Style {
+        match self {
+            Link::Nav { selected } => {
+                if *selected {
+                    button::Style {
+                        text_color: Color::from_rgb(0.10, 0.10, 0.10),
+                        ..button::Style::default()
+                    }
+                } else {
+                    button::Style {
+                        text_color: Color::from_rgb(0.40, 0.40, 0.40),
+                        ..button::Style::default()
+                    }
+                }
+            }
         }
-      }
     }
-  }
 }
