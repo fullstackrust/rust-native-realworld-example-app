f7a3097d43202332c09d0cf8797259e9c593e1bb
Wesley Parr <wesley@cryptoquick.com>
2020-02-01 20:01:23 -0700

    debug content

--- src/view/nav.rs
+++ src/view/nav.rs
@@ -1,4 +1,7 @@
-use iced::{button, Button, Column, Font, HorizontalAlignment, Row, Text};
+use iced::{
+    button, scrollable, Align, Button, Color, Column, Container, Element, Font,
+    HorizontalAlignment, Length, Row, Scrollable, Text,
+};
 
 use crate::message::Message;
 use crate::page::Page;
@@ -9,6 +12,8 @@ pub struct Nav {
     home_button: button::State,
     sign_in_button: button::State,
     sign_up_button: button::State,
+    scroll: scrollable::State,
+    debug: bool,
 }
 
 impl Nav {
@@ -17,14 +22,18 @@ impl Nav {
             home_button: button::State::new(),
             sign_in_button: button::State::new(),
             sign_up_button: button::State::new(),
+            scroll: scrollable::State::new(),
+            debug: false,
         }
     }
 
-    pub fn view(&mut self, current_page: Page) -> Row<Message> {
+    pub fn view(&mut self, current_page: Page) -> Column<Message> {
         let Nav {
             home_button,
             sign_in_button,
             sign_up_button,
+            scroll,
+            debug,
         } = self;
 
         let nav_button = |state, label, link_page, current_page| {
@@ -36,39 +45,78 @@ impl Nav {
             button.on_press(Message::PageChanged(link_page)).padding(6)
         };
 
+        let content: Element<_> = Column::new()
+            .max_width(540)
+            .spacing(20)
+            .padding(20)
+            // .push(steps.view(self.debug).map(Message::StepMessage))
+            // .push(controls)
+            .into();
+
+        let content = if self.debug {
+            content.explain(Color::BLACK)
+        } else {
+            content
+        };
+
+        let scrollable =
+            Scrollable::new(scroll).push(Container::new(content).width(Length::Fill).center_x());
+
         // Font
-        const LOGO_FONT: Font = Font::External {
+        const BRAND_FONT: Font = Font::External {
             name: "TitilliumWeb-Bold",
             bytes: include_bytes!("../resources/TitilliumWeb-Bold.ttf"),
         };
-
-        Row::new()
+        Column::new()
             .push(
-                Column::new().spacing(10).push(
-                    Text::new("conduit")
-                        .color([0.361, 0.722, 0.361])
-                        .font(LOGO_FONT)
-                        .size(48)
-                        .horizontal_alignment(HorizontalAlignment::Center),
-                ),
+                Row::new()
+                    .push(
+                        Column::new().spacing(10).push(
+                            Text::new("conduit")
+                                .color([0.361, 0.722, 0.361])
+                                .font(BRAND_FONT)
+                                .size(24), //.horizontal_alignment(HorizontalAlignment::Center),
+                        ),
+                    )
+                    .push(
+                        Row::new()
+                            .spacing(10)
+                            .push(nav_button(home_button, "Home", Page::Home, current_page))
+                            .push(nav_button(
+                                sign_in_button,
+                                "Sign in",
+                                Page::SignIn,
+                                current_page,
+                            ))
+                            .push(nav_button(
+                                sign_up_button,
+                                "Sign up",
+                                Page::SignUp,
+                                current_page,
+                            )),
+                    )
+                    .spacing(250),
             )
             .push(
                 Row::new()
-                    .spacing(10)
-                    .push(nav_button(home_button, "Home", Page::Home, current_page))
-                    .push(nav_button(
-                        sign_in_button,
-                        "Sign in",
-                        Page::SignIn,
-                        current_page,
-                    ))
-                    .push(nav_button(
-                        sign_up_button,
-                        "Sign up",
-                        Page::SignUp,
-                        current_page,
-                    )),
+                    .spacing(20)
+                    .align_items(Align::Center)
+                    .push(
+                        Column::new().spacing(10).push(
+                            Text::new("conduit")
+                                .color([0.361, 0.722, 0.361])
+                                .font(BRAND_FONT)
+                                .size(48)
+                                .horizontal_alignment(HorizontalAlignment::Center),
+                        ),
+                    )
+                    .spacing(0),
             )
-            .spacing(80)
+            .push(
+                Container::new(scrollable)
+                    .height(Length::Fill)
+                    .center_y()
+                    .into(),
+            )
     }
 }
