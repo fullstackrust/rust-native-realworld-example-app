d46eb4b6010ee18a234324f8a97acfede18aa9cf
Wesley Parr <wesley@cryptoquick.com>
2020-02-03 13:08:49 -0700

    tag button list

--- src/view/home.rs
+++ src/view/home.rs
@@ -1,7 +1,51 @@
-use iced::{Column, Text};
+use iced::{ Button,Color, Column, HorizontalAlignment, Text};
 
 use crate::message::Message;
 
 pub fn new<'a>() -> Column<'a, Message> {
+    let tag_data = vec![
+        "but",
+        "test",
+        "dragons",
+        "training",
+        "tags",
+        "as",
+        "coffee",
+        "animation",
+        "baby",
+        "flowers",
+        "money",
+        "caramel",
+        "cars",
+        "japan",
+        "tag1",
+        "happiness",
+        "sugar",
+        "clean",
+        "cookies",
+        "sushi",
+    ];
+
+    // reduced to newColumn
+    fn new_column(data: Vec<&str>,  is_secure) -> Column<Message> {
+        Column::new().padding(10).spacing(5).push(data.iter().fold(
+            Column::new().padding(5).spacing(10),
+            |choices, language| {
+                choices.push(
+                    Button::new(
+                    state,
+                    Text::new(language)
+                        .color(Color::WHITE)
+                        .horizontal_alignment(HorizontalAlignment::Center),
+                )
+                .padding(12)
+                //.border_radius(12)
+                .min_width(100))
+            },
+        ))
+    }
+
+    let email_row = new_column(tag_data, true);
+
     Column::new().push(Text::new("Home Page"))
 }
