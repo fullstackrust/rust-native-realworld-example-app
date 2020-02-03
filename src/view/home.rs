6cc5eb7f39055d17c593135c0aa71b0cfddd3b2a
Hunter Trujillo <hunter@cryptoquick.com>
2020-02-03 13:14:34 -0700

    fix

--- src/view/home.rs
+++ src/view/home.rs
@@ -1,4 +1,4 @@
-use iced::{ Button,Color, Column, HorizontalAlignment, Text};
+use iced::{Button, Color, Column, HorizontalAlignment, Text};
 
 use crate::message::Message;
 
@@ -27,22 +27,23 @@ pub fn new<'a>() -> Column<'a, Message> {
     ];
 
     // reduced to newColumn
-    fn new_column(data: Vec<&str>,  is_secure) -> Column<Message> {
-        Column::new().padding(10).spacing(5).push(data.iter().fold(
-            Column::new().padding(5).spacing(10),
-            |choices, language| {
-                choices.push(
-                    Button::new(
-                    state,
-                    Text::new(language)
-                        .color(Color::WHITE)
-                        .horizontal_alignment(HorizontalAlignment::Center),
-                )
-                .padding(12)
-                //.border_radius(12)
-                .min_width(100))
-            },
-        ))
+    fn new_column(data: Vec<&str>, is_secure: bool) -> Column<Message> {
+        // Column::new().padding(10).spacing(5).push(data.iter().fold(
+        //     Column::new().padding(5).spacing(10),
+        //     |choices, language| {
+        //         choices.push(
+        //             Button::new(
+        //             state,
+        //             Text::new(language)
+        //                 .color(Color::WHITE)
+        //                 .horizontal_alignment(HorizontalAlignment::Center),
+        //         )
+        //         .padding(12)
+        //         //.border_radius(12)
+        //         .min_width(100))
+        //     },
+        // ))
+        Column::new().push(Text::new("home"))
     }
 
     let email_row = new_column(tag_data, true);
