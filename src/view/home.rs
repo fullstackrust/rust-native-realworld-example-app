3cc786ed4c03bb12036f66e0f66d25358bc72ef2
Wesley Parr <wesley@cryptoquick.com>
2020-02-03 22:51:46 -0700

    refactor home

--- src/view/home.rs
+++ src/view/home.rs
@@ -1,52 +1,70 @@
-use iced::{Button, Color, Column, HorizontalAlignment, Text};
+use iced::{button, Button, Color, Column, HorizontalAlignment, Text};
 
 use crate::message::Message;
+use crate::page::Page;
+//use crate::style;
 
-pub fn new<'a>() -> Column<'a, Message> {
-    let tag_data = vec![
-        "but",
-        "test",
-        "dragons",
-        "training",
-        "tags",
-        "as",
-        "coffee",
-        "animation",
-        "baby",
-        "flowers",
-        "money",
-        "caramel",
-        "cars",
-        "japan",
-        "tag1",
-        "happiness",
-        "sugar",
-        "clean",
-        "cookies",
-        "sushi",
-    ];
+#[derive(Debug, Default, Clone)]
+pub struct Home {
+    sign_in_button: button::State,
+}
 
-    // reduced to newColumn
-    fn new_column(data: Vec<&str>, is_secure: bool) -> Column<Message> {
-        // Column::new().padding(10).spacing(5).push(data.iter().fold(
-        //     Column::new().padding(5).spacing(10),
-        //     |choices, language| {
-        //         choices.push(
-        //             Button::new(
-        //             state,
-        //             Text::new(language)
-        //                 .color(Color::WHITE)
-        //                 .horizontal_alignment(HorizontalAlignment::Center),
-        //         )
-        //         .padding(12)
-        //         //.border_radius(12)
-        //         .min_width(100))
-        //     },
-        // ))
-        Column::new().push(Text::new("home"))
+impl Home {
+    pub fn new() -> Home {
+        Home {
+            sign_in_button: button::State::new(),
+        }
     }
 
-    let email_row = new_column(tag_data, true);
+    pub fn view(&mut self, Home:sign_in_button, current_page: Page) -> Column<Message> {
+        let Home { sign_in_button } = self;
+        let state = Home: sign_in_button;
+        let tag_data = vec![
+            "but",
+            "test",
+            "dragons",
+            "training",
+            "tags",
+            "as",
+            "coffee",
+            "animation",
+            "baby",
+            "flowers",
+            "money",
+            "caramel",
+            "cars",
+            "japan",
+            "tag1",
+            "happiness",
+            "sugar",
+            "clean",
+            "cookies",
+            "sushi",
+        ];
 
-    Column::new().push(Text::new("Home Page"))
+        // reduced to newColumn
+        fn new_column(data: Vec<&str>, is_secure: bool) -> Column<Message> {
+            Column::new().padding(10).spacing(5).push(data.iter().fold(
+                Column::new().padding(5).spacing(10),
+                |choices, language| {
+                    choices.push(
+                        Button::new(
+                            state,
+                            Text::new(language.to_string())
+                                .color(Color::WHITE)
+                                .horizontal_alignment(HorizontalAlignment::Center),
+                        )
+                        .padding(12)
+                        //.border_radius(12)
+                        .min_width(100),
+                    )
+                },
+            ))
+            //Column::new().push(Text::new("home"))
+        }
+
+        let email_row = new_column(tag_data, true);
+
+        Column::new().push(Text::new("Home Page"))
+    }
 }
